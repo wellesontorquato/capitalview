c0389f6b80295ed5f5935242108cac26c05b8499
wellesontorquato <welleson09@hotmail.com>
2025-09-02 02:07:45 -0300

    Ajuste recibo

--- resources/views/recibos/emprestimo.blade.php
+++ resources/views/recibos/emprestimo.blade.php
@@ -70,7 +70,7 @@
   .signature-label { font-size:11px; color:#555; }
 
   footer {
-    margin-top:24px; text-align:center; font-size:10px; color:#888;
+    margin-top:30px; text-align:center; font-size:10px; color:#888;
     padding-top:10px; border-top:1px solid #e0e0e0;
   }
   footer a { color:#555; text-decoration:none; font-weight:700; }
